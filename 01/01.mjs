3207c0de66e6483a3da86f6836eaa95b627898a0
Kevin Lin <kevin.lin.p@gmail.com>
2022-12-03 00:32:10 -0500

    01 done

---
+++ 01/01.mjs
@@ -0,0 +1,35 @@
+import * as fs from 'fs'
+import * as readline from 'readline'
+
+function parseElves() {
+  const file = fs.readFileSync('part-1-input.txt', {encoding: 'utf8'}).trim()
+
+  let elfStrings = file.split('\n\n')
+  let elves = elfStrings.map((elfString) => {
+    const numStrings = elfString.split('\n')
+    const nums = numStrings.map((numString) => {
+      return parseInt(numString)
+    })
+
+    return nums
+  })
+
+  console.log(elves)
+
+  return elves;
+}
+
+const elves = parseElves()
+const elfTotals = elves.map((elf) => {
+  return elf.reduce((acc, current) => acc + current)
+})
+
+console.log(elfTotals)
+
+const max = elfTotals.reduce((acc, current) => acc > current ? acc : current)
+console.log(max)
+
+const maxThree = elfTotals.sort((a, b) => a > b).slice(-3)
+console.log(maxThree)
+const sum = maxThree.reduce((acc, current) => acc + current)
+console.log(sum)
